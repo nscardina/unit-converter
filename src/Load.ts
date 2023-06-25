03c9f037e2c78a4d08744c3c6fb3579d965f66f2
Noah Scardina <88603144+nscardina@users.noreply.github.com>
2023-06-25 15:40:59 -0400

    Updates

--- src/Load.ts
+++
@@ -1,8 +0,0 @@
-const Json: (path: URL) => Promise<any> = 
-(path: URL) => fetch(path)
-        .catch(reason => { throw new Error(reason) })
-        .then(value => value.text())
-        .catch(reason => { throw new Error(reason) })
-        .then(value => JSON.parse(value))
-
-export default { Json }
--- src/misc.ts
+++
@@ -1,3 +0,0 @@
-export function capitalizeFirstLetter(str: string) {
-    return (str.length > 0) ? `${str.charAt(0).toUpperCase()}${str.substring(1)}` : ''
-}
